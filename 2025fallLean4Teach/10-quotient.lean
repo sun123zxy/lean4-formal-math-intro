9fc9e38e76e792d36bb4c5688491ff0267c4288e
sun123zxy <304244450@qq.com>
2025-11-26 02:25:42 +0800

    upd

---
+++ 2025fallLean4Teach/10-quotient.lean
@@ -0,0 +1,14 @@
+/-
+# Quotients
+
+After building up the theory of groups, homorphisms, and subgroups,
+we are now ready to define quotient groups.
+
+In fact, quotients are so fundamental that
+Lean makes them a primitive way of constructing new types.
+
+We shall first illustrate the general quotient construction in Lean,
+and then specialize it to quotient groups.
+
+At the end of the journey, we show the first isomorphism theorem for groups as promised.
+-/
