7b7668df6619fdbe26358871e708ad9072d7f313
sun123zxy <304244450@qq.com>
2025-11-27 17:16:40 +0800

    upd

--- 2025fallLean4Teach/10-quotient.lean
+++ 2025fallLean4Teach/10-quotient.lean
@@ -1,3 +1,5 @@
+import Mathlib
+
 /-
 # Quotients
 
@@ -12,3 +14,43 @@ and then specialize it to quotient groups.
 
 At the end of the journey, we show the first isomorphism theorem for groups as promised.
 -/
+
+/-
+## Quotient Sets
+
+We still build up the theory from sets.
+First, we need to define equivalence relations on a type `α`.
+As you can guess, a binary relation `r` is just a `α → α → Prop`.
+-/
+section
+
+variable {α : Type*} {r : α → α → Prop} (a b c : α)
+
+/-
+`Equivalence r` is a bundled structure that packages the three properties:
+
+- reflexivity of `r`
+- symmetry of `r`
+- transitivity of `r`
+-/
+variable (r_equiv : Equivalence r)
+
+example : r a a := by exact r_equiv.refl _
+example : r a b → r b a := by exact r_equiv.symm
+example : r a b → r b c → r a c := by exact r_equiv.trans
+
+/-
+In mathmatics we often denote equivalence relations by `~`.
+In Lean, we can also use `≈` as notation for equivalence relations,
+once we register the `Setoid` instance for `α`.
+
+The `Setoid` typeclass is a bundle of
+
+- an equivalence relation `r` on `α`
+- the proof that `r` is an equivalence relation.
+-/
+instance r_setoid : Setoid α where
+  r := r
+  iseqv := r_equiv
+
+end
